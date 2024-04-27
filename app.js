eb173e98c2dd192124e3ab6dee8fb3bfe32c6547
VictorCottar <victorcottarms@outlook.com>
2024-04-26 22:46:28 -0300

    Detalhes da Vaga

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express    = require('express');
-const app        = express();
-const db         = require('./db/connection');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const app = express();
+const path = require('path');
+const db = require('./db/connection');
 const bodyParser = require('body-parser');
-const router = require('./routes/jobs');
+const Job = require('./models/Job');
+const { where, Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
+
 
 const PORT = 3000;
 
-app.listen(PORT, function() {
-    console.log(`O express está rodando na porta ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`O express está rodando na porta ${PORT}`);
 })
 
 
 // BODY PARSER
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+// HANDLE BARS
+app.engine('handlebars', engine());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
+
+// STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
 
 // DB CONNECTION
 db
@@ -28,12 +42,46 @@ db
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send("Está funcionando");
+
+  let search = req.query.job;
+  let query = '%' + search + '%';
+
+  if (!search) {
+    Job.findAll({
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    })
+      .then(jobs => {
+
+        res.render('index', {
+          jobs
+        });
+      })
+      .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: { title: { [Op.like]: query } },
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    })
+
+      .then(jobs => {
+        res.render('index', {
+          jobs, search
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+
 })
 
-router.post('/test', (req, res) => {
-    res.send("Está ffafafuncionando");
+app.get('/jobs/add', (req, res) => {
+  res.render('jobs');
 })
+
 
 // JOBS ROUTES 
 app.use('/jobs', require('./routes/jobs'));
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
+const { where } = require('sequelize');
 
 
+// DETALHES DA VAGA
+router.get('/details/:id', (req, res) => Job.findOne ({
+    where: {id: req.params.id}
+}).then(job => {
+
+    res.render('details', {
+        job
+    });
+
+}).catch(err => console.log(err)));
+
+
+// ADICIONAR JOB
 router.post('/add', (req, res) => {
 
     let { title, salary, company, description, email, new_job } = req.body;
